6c9148b74288ceae2a3a0b96bf54dfd25bb767b0
daffaputra24 <daffaputrawibowo2408@gmail.com>
2022-10-04 12:10:04 +0700

    add UI

---
+++ app/Views/landing_page.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sportify - Booking Lapangan Online</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+    <link rel="shortcut icon" href="../assets/images/ball.svg" type="image/x-icon">
+</head>
+
+<body>
+    <section>
+        <nav class="navbar navbar-expand-lg navbar-light pt-3 pt-lg-3">
+            <div class="container">
+                <a class="navbar-brand p-lg-3 d-flex" href="#">
+                    <img src="../assets/images/ball.svg" alt="Brand Icon" class="logo-brand">
+                    <h5 class="mb-0 ms-2">Sportify</h5>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+                    <a href="login.php"
+                        class="btn btn-outline-primary px-4 py-2 me-lg-2 ms-lg-auto mt-4 mt-lg-0 d-block d-lg-inline-block ">Masuk</a>
+                    <a href="register.php" class="btn btn-primary px-4 py-2 mt-3 mt-lg-0 d-block d-lg-inline-block">Daftar</a>
+                </div>
+            </div>
+        </nav>
+
+        <section class="container">
+            <img src="../assets/images/bg-header.svg" alt="bg-header" class="img-header d-none d-lg-block">
+            <div class="row mx-0" id="header">
+                <div class="col-xl-7">
+                    <div class="title">
+                        <h1>
+                            Booking Lapangan dalam <span style="color: #ff4757">Genggaman</span>
+                        </h1>
+                    </div>
+                    <div class="d-flex stat">
+                        <div class="text-left me-4 me-md-5">
+                            <h2>
+                                200
+                            </h2>
+                            <p>
+                                Lapangan
+                            </p>
+                        </div>
+                        <div class="vr"></div>
+                        <div class="px-md-5 px-4">
+                            <h2>
+                                12
+                            </h2>
+                            <p>
+                                Kota
+                            </p>
+                        </div>
+                        <div class="vr"></div>
+                        <div class="ms-4 ms-md-5">
+                            <h2>
+                                2000+
+                            </h2>
+                            <p>
+                                Pesanan
+                            </p>
+                        </div>
+                    </div>
+                    <div class="stat">
+                        <p class="descript-head">
+                            Tak perlu khawatir tidak kebagian lapangan. Ribuan orang dapat berolahraga dengan tenang
+                            karena bisa booking lapangan dalam hitungan menit.
+                        </p>
+                    </div>
+                    <div>
+                        <a href="" class="btn btn-primary mt-5 px-5 py-3 shadow">
+                            Cari Lapangan
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-5 my-auto">
+                    <div class="gallery row p-md-4 section scrolling-wrapper flex-row flex-nowrap">
+                        <!-- CARD 1 -->
+                        <div class="card-hotel-carousel">
+                            <div class="image-placeholder">
+                                <img src="../assets/images/futsal.jpg" alt="images" />
+                            </div>
+                            <div class="card-details">
+                                <div class="caption">Ghinan Futsal</div>
+                                <span class="sub-caption">150m</span>
+                            </div>
+                            <div class="bottom-text d-flex flex-row justify-content-between">
+                                <div class="price-content flex-grow-1">
+                                    <span>Mulai dari</span> <span class="price">Rp.70.000</span>
+                                </div>
+                                <div class="rating d-flex align-items-center">
+                                    <img src="../assets/images/star-yellow.svg" alt="star" />
+                                    <span>4.0</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END CARD 1 -->
+                        <!-- CARD 2 -->
+                        <div class="card-hotel-carousel">
+                            <div class="image-placeholder">
+                                <img src="../assets/images/badminton.jpg" alt="images" />
+                            </div>
+                            <div class="card-details">
+                                <div class="caption">IKS Badminton</div>
+                                <span class="sub-caption">1500m</span>
+                            </div>
+                            <div class="bottom-text d-flex flex-row justify-content-between">
+                                <div class="price-content flex-grow-1">
+                                    <span>Mulai dari</span> <span class="price">Rp.40.000</span>
+                                </div>
+                                <div class="rating d-flex align-items-center">
+                                    <img src="../assets/images/star-yellow.svg" alt="star" />
+                                    <span>3.7</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END CARD 2 -->
+                        <!-- CARD 3 -->
+                        <div class="card-hotel-carousel">
+                            <div class="image-placeholder">
+                                <img src="../assets/images/basket.jpeg" alt="images" />
+                            </div>
+                            <div class="card-details">
+                                <div class="caption">Basket Dino</div>
+                                <span class="sub-caption">2000m</span>
+                            </div>
+                            <div class="bottom-text d-flex flex-row justify-content-between">
+                                <div class="price-content flex-grow-1">
+                                    <span>Mulai dari</span> <span class="price">Rp.80.000</span>
+                                </div>
+                                <div class="rating d-flex align-items-center">
+                                    <img src="https://api.elements.buildwithangga.com/storage/files/2/assets/Header/Header-House/star-yellow.svg"
+                                        alt="star" />
+                                    <span>4.8</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END CARD 3 -->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </section>
+    <section class="content text-center">
+        <div class="container">
+            <div class="row content">
+                <div class="col-12 px-md-0 my-auto">
+                    <div class="tagline">
+                        RUMUS SEDERHANA
+                    </div>
+                    <div class="headline mt-3">
+                        3-Langkah Mudah <br class="d-none d-md-block">
+                        Booking Lapangan
+                    </div>
+                    <div class="row benefits">
+                        <div class="col-md-4 mt-md-0">
+                            <div class="rectangle mx-auto px-1">
+                                <img src="../assets/images/ic_signup.png" width="106" alt="benefits-1" class="img-fluid">
+                                <div class="headline-benefit">
+                                    Daftar
+                                </div>
+                                <div class="subheadline-benefit mt-2">
+                                    Daftarkan akun Anda <br class="d-none d-md-block">
+                                    dan bersiap untuk menjelajah
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mt-5 mt-md-0">
+                            <div class="rectangle mx-auto px-1">
+                                <img src="../assets/images/ic_booking.png" alt="benefits-1" class="img-fluid" width="106">
+                                <div class="headline-benefit">
+                                    Booking Lapangan
+                                </div>
+                                <div class="subheadline-benefit mt-2">
+                                    Pilih lapangan dan <br class="d-none d-md-block">
+                                    tanggal untuk bermain
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mt-5 mt-md-0">
+                            <div class="rectangle mx-auto px-1">
+                                <img src="../assets/images/ic_pay.png" alt="benefits-1" class="img-fluid" width="106">
+                                <div class="headline-benefit">
+                                    Bayar
+                                </div>
+                                <div class="subheadline-benefit mt-2">
+                                    Bayar biaya booking saja <br class="d-none d-md-block">
+                                    sisanya di lapangan
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-inline-block d-md-flex justify-content-center mt-5">
+                        <div class="button-header me-0 me-md-3">
+                            <a href="#" class="btn btn-primary px-4 py-2">Mulai Sekarang</a>
+                        </div>
+                        <div class="button-header">
+                            <a href="#client" class="btn btn-outline-primary px-4 py-2 mt-2 mt-md-0">Baca Testimoni</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="left-circle">
+
+    </section>
+
+    <section class="client" id="client">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center">
+                        Dipercaya lebih dari 100 orang olahragawan
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="container review-section">
+            <div class="row">
+                <div class="col-md-5 px-5 mt-4 mt-md-0 d-flex justify-content-center">
+                    <img src="../assets/images/golf-man.jpg" alt="" class="img-fluid img-gym">
+                </div>
+                <div class="col-md-7 mt-4 mt-md-0 pe-lg-5">
+                    <div class="text-end mb-3">
+                        <img src="../assets/images/petik.png" alt="mark" class="img-fluid">
+                    </div>
+                    <p class="review">
+                        Semenjak saya memakai Sportify, saya tidak takut lagi tidak kebagian lapangan. Cukup booking dan
+                        bayar lewat website ini. Terima kasih Sportify.
+                    </p>
+                    <h5 class="mt-5">
+                        Jamal Akhyer
+                    </h5>
+                    <p class="company">
+                        Atlet Futsal
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="page-footer font-small blue">
+        <div class="container text-md-left">
+            <div class="row d-flex flex-column-reverse flex-lg-row">
+                <div class="col-lg-6 mt-md-0 mt-3 address align mb-5 mb-lg-0">
+                    <div class="logo font-red-hat-display">
+                        Sportify
+                    </div>
+
+                    <div class="copyright font-red-hat-display align-self-end">
+                        2022 All rights reserved.
+                    </div>
+                </div>
+                <div class="row col-lg-6 nav-footer">
+                    <div class="col-md-4 mb-md-0 mb-3">
+                        <p>
+                            Penggunaan
+                        </p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Panduan</a>
+                            </li>
+                            <li>
+                                <a href="#!">Metode Pembayaran</a>
+                            </li>
+                            <li>
+                                <a href="#!">Costumer Services</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-3">
+                        <p>
+                            Pengelola Lapangan
+                        </p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Biaya Listing</a>
+                            </li>
+                            <li>
+                                <a href="#!">Ajukan Listing</a>
+                            </li>
+                            <li>
+                                <a href="#!">Panduan</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-3">
+                        <p>
+                            Perusahaan Kami
+                        </p>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Tentang Kami</a>
+                            </li>
+                            <li>
+                                <a href="#!">Karir</a>
+                            </li>
+                            <li>
+                                <a href="#!">Blog & Berita</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 
     <!-- STYLES -->
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style {csp-style-nonce}>
         * {
             transition: background-color 300ms ease, color 300ms ease;
